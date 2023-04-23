b7ecf51fd22c3add788658df852eddaf651f4091
rej <rzioma@acm.org>
2023-04-23 19:31:17 +0200

    2 layers with only 8 hidden units

--- src/test.py
+++ src/test.py
@@ -2,19 +2,53 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 
-
-MAX_TEST_NEURONS = 8
-weights = [0xff, 0xff, 0xff, 0xff, 0x55, 0xAA, 0x0F, 0xC3]
-bias =    [   0,    1,    2,    7,    3,    3,    3,    3]
+TEST = 1
 
 def neuron(id, x):
     return bin(x & weights[id]).count("1") > bias[id];
+
+if TEST == 0: # 1 layer
+    MAX_TEST_OUTPUTS = 8
+    MAX_NEURON_PARAMS_TO_UPLOAD = 8
+    weights = [0xff, 0xff, 0xff, 0xff, 0x55, 0xAA, 0x0F, 0xC3]
+    bias =    [   0,    1,    2,    7,    3,    3,    3,    3]
+    bits_w =  [   8,    8,    8,    8,    8,    8,    8,    8]
+    bits_b =  [   3,    3,    3,    3,    3,    3,    3,    3]
+    def output(id, x): return neuron(id, x)
+
+elif TEST == 1: # 2 layers
+    HIDDEN_UNITS = 8
+    MAX_TEST_OUTPUTS = 8
+    MAX_NEURON_PARAMS_TO_UPLOAD = 8+8
+    weights = [0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80,    0xff, 0xff, 0xff, 0xff, 0x55, 0xAA, 0x0F, 0xC3]
+    bias =    [   0,    0,    0,    0,    0,    0,    0,    0,       0,    1,    2,    7,    3,    3,    3,    3]
+    bits_w =  [   8,    8,    8,    8,    8,    8,    8,    8,       8,    8,    8,    8,    8,    8,    8,    8]
+    bits_b =  [   3,    3,    3,    3,    3,    3,    3,    3,       3,    3,    3,    3,    3,    3,    3,    3]
+    def output(id, x):
+        h = 0
+        for n in reversed(range(HIDDEN_UNITS)):
+            h = (h << 1) + neuron(n, x)
+        return neuron(HIDDEN_UNITS+id, h)
+
+elif TEST == 2: # 2 layers
+    HIDDEN_UNITS = 16
+    MAX_TEST_OUTPUTS = 8
+    MAX_NEURON_PARAMS_TO_UPLOAD = 16+8
+    weights = [0xff,0xff,0xff,0xff,0x55,0xAA,0x0F,0xC3,0xff,0xff,0xff,0xff,0x01,0x02,0x04,0x08,     0xffff,0xffff,0xffff,0xffff,0xffff,0xffff,0xffff,0xffff]
+    bias =    [   0,   1,   2,   7,   3,   3,   3,   3,   3,   4,   5,   6,   0,   0,   0,   0,          0,     1,     2,     3,     4,     5,     6,     7]
+    bits_w =  [   8,   8,   8,   8,   8,   8,   8,   8,   8,   8,   8,   8,   8,   8,   8,   8,         16,    16,    16,    16,    16,    16,    16,    16]
+    bits_b =  [   3,   3,   3,   3,   3,   3,   3,   3,   3,   3,   3,   3,   3,   3,   3,   3,          3,     3,     3,     3,     3,     3,     3,     3]
+    def output(id, x):
+        h = 0
+        for n in reversed(range(HIDDEN_UNITS)):
+            h = (h << 1) + neuron(n, x)
+        return neuron(HIDDEN_UNITS+id, h)
 
 def nth_bit(x, n):
     return (x >> n) & 1
 
 @cocotb.test()
-async def test_7seg(dut):
+async def test_tiny_dnn(dut):
     dut._log.info("start")
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
@@ -31,11 +65,11 @@ async def test_7seg(dut):
     #         dut.x.value = nth_bit(bias[n], 0);
     #         await ClockCycles(dut.clk, 1)
 
-    for n in reversed(range(min(MAX_TEST_NEURONS, len(weights)))):
-        for i in reversed(range(3)):
+    for n in reversed(range(min(MAX_NEURON_PARAMS_TO_UPLOAD, len(weights)))):
+        for i in reversed(range(bits_b[n])):
             dut.x.value = nth_bit(bias[n], i);
             await ClockCycles(dut.clk, 1)
-        for i in reversed(range(8)):
+        for i in reversed(range(bits_w[n])):
             dut.x.value = nth_bit(weights[n], i);
             await ClockCycles(dut.clk, 1)
 
@@ -62,5 +96,5 @@ async def test_7seg(dut):
         dut.x.value = x
         await ClockCycles(dut.clk, 10)
         print(dut.out.value)
-        for n in range(min(MAX_TEST_NEURONS, len(weights))):
-            assert int(dut.out[n].value) == neuron(n, x)
+        for n in range(min(MAX_TEST_OUTPUTS, len(weights))):
+            assert int(dut.out[n].value) == output(n, x)
