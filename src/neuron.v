a4ea013dfc50e7630abf26760baa7f1f731a8ff5
rej <rzioma@acm.org>
2023-04-24 22:01:46 +0200

    Now compare with separate popcount module and always_comb

--- src/neuron.v
+++ src/neuron.v
@@ -10,7 +10,7 @@ module neuron #(
     output param_out,
 
     input wire [INPUTS-1:0] inputs,
-    output reg axon
+    output axon
 );
 
     localparam ACCUMULATOR_BITS = $clog2(INPUTS) + 1;
@@ -51,7 +51,7 @@ module neuron #(
             // $display(">> ", param_in);
             // $display("w = ", weights);
             // $display("b = ", bias);
-        end else begin
+        // end else begin
             // accumulator = 0;
             // for  (i = 0; i < INPUTS; i = i + 1)
             //     accumulator = accumulator + (weights[i] & inputs[i]);
@@ -69,9 +69,10 @@ module neuron #(
         end
     end
 
-    // wire [INPUTS-1:0] synapses;
-    // assign synapses = weights & inputs;
-    // popcount #(.INPUTS(8), .COUNTER_BITS(ACCUMULATOR_BITS)) spike_counter(.in(synapses), .count(accumulator));
+    wire [INPUTS-1:0] synapses;
+    assign synapses = weights & inputs;
+    popcount #(.INPUTS(INPUTS), .COUNTER_BITS(ACCUMULATOR_BITS)) spike_counter(.in(synapses), .count(accumulator));
+    assign axon = accumulator > bias;
 
     // always @(*) begin
     //     //axon <= (accumulator > bias);
@@ -83,43 +84,35 @@ module neuron #(
     //     // $display("a = ", accumulator);
     // end
 
-    always @(inputs) begin
-        // synapses <= weights & inputs;
-        // $display("w = ", weights);
-        // $display("b = ", bias);
-        // $display("i = ", inputs);
-        // $display("t = ", synapses);
-        accumulator = 0;
-        for  (i = 0; i < INPUTS; i = i + 1)
-            accumulator = accumulator + (weights[i] & inputs[i]);
-        // $display("accumulator value = ", accumulator);
-        if (USE_CHEAP_BIAS == 1)
-            axon = |(accumulator & bias);
-        else
-            axon = (accumulator > bias);
-    end
+    // always @(inputs) begin
+    //     // synapses <= weights & inputs;
+    //     // $display("w = ", weights);
+    //     // $display("b = ", bias);
+    //     // $display("i = ", inputs);
+    //     // $display("t = ", synapses);
+    //     accumulator = 0;
+    //     for  (i = 0; i < INPUTS; i = i + 1)
+    //         accumulator = accumulator + (weights[i] & inputs[i]);
+    //     // $display("accumulator value = ", accumulator);
+    //     if (USE_CHEAP_BIAS == 1)
+    //         axon = |(accumulator & bias);
+    //     else
+    //         axon = (accumulator > bias);
+    // end
 
-    function [1:0] enc2;
-        input [1:0] in;
-        begin
-            case (in)
-                2'b00: enc2 = 2'd0;
-                2'b01: enc2 = 2'd1;
-                2'b10: enc2 = 2'd1;
-                2'b11: enc2 = 2'd2;
-            endcase
-        end
-    endfunction
 
-    wire [7:0] synapses;
-    wire [1:0] count0;
-    wire [1:0] count1;
-    wire [1:0] count2;
-    wire [1:0] count3;
-    wire [2:0] count4;
-    wire [2:0] count5;
-    wire [3:0] count;
-    assign synapses = weights & inputs;
+    // wire [INPUTS-1:0] synapses;
+    // assign synapses = weights & inputs;
+    // assign axon = (synapses[7]+synapses[6]+synapses[5]+synapses[4]+synapses[3]+synapses[2]+synapses[1]+synapses[0]) > bias;
+    // assign axon = ((synapses[7]+synapses[6])+(synapses[5]+synapses[4]))+((synapses[3]+synapses[2])+(synapses[1]+synapses[0])) > bias;
+
+    // wire [1:0] count0;
+    // wire [1:0] count1;
+    // wire [1:0] count2;
+    // wire [1:0] count3;
+    // wire [2:0] count4;
+    // wire [2:0] count5;
+    // wire [3:0] count;
     // assign count0 = enc2(synapses[1:0]);
     // assign count1 = enc2(synapses[3:2]);
     // assign count2 = enc2(synapses[5:4]);
@@ -132,8 +125,6 @@ module neuron #(
     // assign count5 = count2 + count3;
     // assign count = count4 + count5;
     // assign axon = count > bias;
-    // assign axon = (synapses[7]+synapses[6]+synapses[5]+synapses[4]+synapses[3]+synapses[2]+synapses[1]+synapses[0]) > bias;
-    // assign axon = ((synapses[7]+synapses[6])+(synapses[5]+synapses[4]))+((synapses[3]+synapses[2])+(synapses[1]+synapses[0])) > bias;
 
     // reg [7:0] synapses;
     // reg [1:0] count0;
--- src/popcount.v
+++ src/popcount.v
@@ -8,48 +8,12 @@ module popcount #(
 );
 
     integer i;
-    // always @(in) begin
-    //     count = 0;
-    //     for  (i = 0; i < INPUTS; i = i + 1) begin
-    //         count = count + in[i];
-    //     end
-    // end
-
-    always @(in) begin
-        reg [1:0] count0;
-        reg [1:0] count1;
-        reg [1:0] count2;
-        reg [1:0] count3;
-        reg [2:0] count4;
-        reg [2:0] count5;
-        case (in[1:0])
-            2'b00: count0 = 2'd0;
-            2'b01: count0 = 2'd1;
-            2'b10: count0 = 2'd1;
-            2'b11: count0 = 2'd2;
-        endcase
-        case (in[3:2])
-            2'b00: count1 = 2'd0;
-            2'b01: count1 = 2'd1;
-            2'b10: count1 = 2'd1;
-            2'b11: count1 = 2'd2;
-        endcase
-        case (in[5:4])
-            2'b00: count2 = 2'd0;
-            2'b01: count2 = 2'd1;
-            2'b10: count2 = 2'd1;
-            2'b11: count2 = 2'd2;
-        endcase
-        case (in[7:6])
-            2'b00: count3 = 2'd0;
-            2'b01: count3 = 2'd1;
-            2'b10: count3 = 2'd1;
-            2'b11: count3 = 2'd2;
-        endcase
-
-        count4 = count0 + count1;
-        count5 = count2 + count3;
-        count = count4 + count5;
+    always_comb begin
+        count = 0;
+        for  (i = 0; i < INPUTS; i = i + 1) begin
+            count = count + in[i];
+        end
     end
+
 
 endmodule
