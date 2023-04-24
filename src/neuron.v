cf39c57ced3c5422bf25c7bc41e194d209bc292f
rej <rzioma@acm.org>
2023-04-24 19:04:46 +0200

    Verbose neuron, attempt to debug test failing in GL mode

--- src/neuron.v
+++ src/neuron.v
@@ -39,7 +39,7 @@ module neuron #(
 
     assign param_out = bias[BIAS_BITS-1];
 
-    always_ff @(posedge clk) begin
+    always @(posedge clk) begin
         // load weights & bias in the setup phase
         if (setup) begin
             //bias <= {bias[BIAS_BITS-2:0], weights[INPUTS-1]};
@@ -48,9 +48,25 @@ module neuron #(
             bias[0] <= weights[INPUTS-1];
             weights <= weights << 1;
             weights[0] <= param_in;
-            // $display(">> ", param_in);
-            // $display("w = ", weights);
-            // $display("b = ", bias);
+            $display(">> ", param_in);
+            $display("w = ", weights);
+            $display("b = ", bias);
+        end else begin
+            accumulator = 0;
+            for  (i = 0; i < INPUTS; i = i + 1)
+                accumulator = accumulator + (weights[i] & inputs[i]);
+            // $display("accumulator value = ", accumulator);
+            if (USE_CHEAP_BIAS == 1)
+                axon = |(accumulator & bias);
+            else
+                axon = (accumulator > bias);
+
+            $display("w = ", weights);
+            $display("i = ", inputs);
+            $display("s = ", synapses);
+            $display("b = ", bias);
+            $display("a = ", accumulator);
+
         end
     end
 
@@ -68,55 +84,57 @@ module neuron #(
     //     // $display("a = ", accumulator);
     // end
 
-    always @(inputs) begin
-        // synapses <= weights & inputs;
-        // $display("w = ", weights);
-        // $display("b = ", bias);
-        // $display("i = ", inputs);
-        // $display("t = ", synapses);
-        accumulator = 0;
-        for  (i = 0; i < INPUTS; i = i + 1)
-            accumulator = accumulator + (weights[i] & inputs[i]);
-        // $display("accumulator value = ", accumulator);
-        if (USE_CHEAP_BIAS == 1)
-            axon <= |(accumulator & bias);
-        else
-            axon <= (accumulator > bias);
-    end
+    // always @(inputs) begin
+    //     // synapses <= weights & inputs;
+    //     // $display("w = ", weights);
+    //     // $display("b = ", bias);
+    //     // $display("i = ", inputs);
+    //     // $display("t = ", synapses);
+    //     accumulator = 0;
+    //     for  (i = 0; i < INPUTS; i = i + 1)
+    //         accumulator = accumulator + (weights[i] & inputs[i]);
+    //     // $display("accumulator value = ", accumulator);
+    //     if (USE_CHEAP_BIAS == 1)
+    //         axon <= |(accumulator & bias);
+    //     else
+    //         axon <= (accumulator > bias);
+    // end
 
-    // function [1:0] enc2;
-    //     input [1:0] in;
-    //     begin
-    //         case (in)
-    //             2'b00: enc2 = 2'd0;
-    //             2'b01: enc2 = 2'd1;
-    //             2'b10: enc2 = 2'd1;
-    //             2'b11: enc2 = 2'd2;
-    //         endcase
-    //     end
-    // endfunction
+    function [1:0] enc2;
+        input [1:0] in;
+        begin
+            case (in)
+                2'b00: enc2 = 2'd0;
+                2'b01: enc2 = 2'd1;
+                2'b10: enc2 = 2'd1;
+                2'b11: enc2 = 2'd2;
+            endcase
+        end
+    endfunction
 
-    // wire [7:0] synapses;
-    // wire [1:0] count0;
-    // wire [1:0] count1;
-    // wire [1:0] count2;
-    // wire [1:0] count3;
-    // wire [2:0] count4;
-    // wire [2:0] count5;
-    // wire [3:0] count;
-    // assign synapses = weights & inputs;
+    wire [7:0] synapses;
+    wire [1:0] count0;
+    wire [1:0] count1;
+    wire [1:0] count2;
+    wire [1:0] count3;
+    wire [2:0] count4;
+    wire [2:0] count5;
+    wire [3:0] count;
+    assign synapses = weights & inputs;
+    // assign count0 = enc2(synapses[1:0]);
+    // assign count1 = enc2(synapses[3:2]);
+    // assign count2 = enc2(synapses[5:4]);
+    // assign count3 = enc2(synapses[7:6]);
     // assign count0 = synapses[0] + synapses[1];
     // assign count1 = synapses[2] + synapses[3];
     // assign count2 = synapses[4] + synapses[5];
     // assign count3 = synapses[6] + synapses[7];
-    // // assign count0 = enc2(synapses[1:0]);
-    // // assign count1 = enc2(synapses[3:2]);
-    // // assign count2 = enc2(synapses[5:4]);
-    // // assign count3 = enc2(synapses[7:6]);
     // assign count4 = count0 + count1;
     // assign count5 = count2 + count3;
-    // // assign count = enc2(synapses[1:0]) + enc2(synapses[3:2]) + enc2(synapses[5:4]) + enc2(synapses[7:6]);
     // assign count = count4 + count5;
+    // assign axon = count > bias;
+    //assign axon = synapses[7]+synapses[6]+synapses[5]+synapses[4]+synapses[3]+synapses[2]+synapses[1]+synapses[0] > bias;
+    //assign axon = ((synapses[7]+synapses[6])+(synapses[5]+synapses[4]))+((synapses[3]+synapses[2])+(synapses[1]+synapses[0])) > bias;
 
     // reg [7:0] synapses;
     // reg [1:0] count0;
