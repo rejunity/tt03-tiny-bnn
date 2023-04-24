56a96e025427d2a87071e15e331ec47b3e0bb083
rej <rzioma@acm.org>
2023-04-24 18:06:15 +0200

    Lets see if GL test will fail on 1 layer network

--- src/test.py
+++ src/test.py
@@ -2,7 +2,7 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 
-TEST = 1
+TEST = 0
 USE_CHEAP_BIAS = False # True
 
 def neuron(id, x):
