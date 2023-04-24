f7bff60dbdc620a258eb6aca2dfcd227e61dbdbb
rej <rzioma@acm.org>
2023-04-24 22:25:04 +0200

    Manually unrolled variants for popcount

--- src/popcount.v
+++ src/popcount.v
@@ -8,10 +8,22 @@ module popcount #(
 );
 
     integer i;
-    always_comb begin
-        count = 0;
-        for  (i = 0; i < INPUTS; i = i + 1) begin
-            count = count + in[i];
+    always @(*) begin
+        if (INPUTS == 4) begin
+            count =                                 in[3] + in[2] + in[1] + in[0];
+        end else if (INPUTS == 8) begin
+            count = in[7] + in[6] + in[5] + in[4] + in[3] + in[2] + in[1] + in[0];
+        end else if (INPUTS == 10) begin
+            count =                                        in[10] + in[9] + in[8] +
+                    in[7] + in[6] + in[5] + in[4] + in[3] + in[2] + in[1] + in[0];
+        end else if (INPUTS == 16) begin
+            count = in[15]+in[14] +in[13] +in[12] +in[11] +in[10] + in[9] + in[8] +
+                    in[7] + in[6] + in[5] + in[4] + in[3] + in[2] + in[1] + in[0];
+        end else begin
+            count = 0;
+            for  (i = 0; i < INPUTS; i = i + 1) begin
+                count = count + in[i];
+            end
         end
     end
 
